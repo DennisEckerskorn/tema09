81257af19c0d7b151bc8f84e78fc7ebf8d849d6a
DennisEckerskorn <45369024+DennisEckerskorn@users.noreply.github.com>
2024-04-11 23:30:00 +0200

    Add Ejer02 - Read decimal numbers, Improved Generic DynamicArray with getMaxValue() method

--- src/main/java/com/denniseckerskorn/dynamicarray/GenericDynamicArray.java
+++ src/main/java/com/denniseckerskorn/dynamicarray/GenericDynamicArray.java
@@ -1,33 +1,65 @@
 
 package com.denniseckerskorn.dynamicarray;
 
-
 import java.util.Arrays;
 
+/**
+ * Clase que representa un array dinámico genérico.
+ * Permite almacenar elementos de cualquier tipo y ajustar dinámicamente su tamaño según sea necesario.
+ *
+ * @param <T> el tipo de elementos que se almacenarán en el array dinámico.
+ */
 public class GenericDynamicArray<T> {
     private static final float GROW_FACTOR = 2f;
     private static final int DEFAULT_CAPACITY = 20;
     private Object[] data;
     private int elementCount;
 
-    //Constructor
+    /*** Constructor que crea un nuevo GenericDynamicArray con la capacidad inicial especificada.
+     * @param capacidadInicial la capacidad inicial del array dinámico.
+     */
     public GenericDynamicArray(int capacidadInicial) {
         data = new Object[capacidadInicial];
         elementCount = 0;
     }
 
+    /**
+     * Constructor que crea un nuevo GenericDynamicArray con la capacidad por defecto.
+     */
     public GenericDynamicArray() {
         this(DEFAULT_CAPACITY);
     }
 
     /**
-     * Método para añadir un valor double a la última posición del array.
-     * Primero se comprueba si el array está lleno, si no se añade el elemento.
-     * @param value double, valor a añadir al array.
-     * @return true si se ha realizado correctamente.
+     * Método que busca el valor máximo del array.
+     *
+     * @return el valor máximo del array, o null si el array está vacío.
+     */
+    @SuppressWarnings("unchecked")
+    public T getMaxValue() {
+        if (elementCount == 0) {
+            return null;
+        }
+
+        T maxValue = (T) data[0];
+
+        for (int i = 1; i < data.length; i++) {
+            Comparable<T> currentValue = (Comparable<T>) data[i];
+            if (currentValue.compareTo(maxValue) > 0) {
+                maxValue = (T) data[i];
+            }
+        }
+        return maxValue;
+    }
+
+    /**
+     * Método para añadir un valor al final del array dinámico.
+     *
+     * @param value el valor a añadir al array dinámico.
+     * @return true si se ha añadido correctamente, false si no.
      */
     public boolean add(T value) {
-        if(isFull()) {
+        if (isFull()) {
             expandArray();
         }
         data[elementCount++] = value;
@@ -35,36 +67,45 @@ public class GenericDynamicArray<T> {
     }
 
     /**
-     * Método que recibe el valor a insertar en el índice indicado.
-     * Una vez se encuentre la posición del índice, se mueven todos los valores de la derecha una posición a la derecha.
-     * El bucle itera desde el final hasta el índice indicado.
-     * @param index int, índice donde se inserta el valor.
-     * @param value double, valor que se inserta.
-     * @return devuelve true si el valor se ha insertado correctamente y false si no se ha podido hacer correctamente.
+     * Método que añade un valor en el índice especificado del array dinámico.
+     *
+     * @param index el índice donde se debe añadir el valor.
+     * @param value el valor a añadir al array dinámico.
+     * @return true si se ha añadido correctamente, false si no.
      */
     public boolean add(int index, T value) {
-        if(index >= elementCount || index < 0) {
+        if (index >= elementCount || index < 0) {
             return false;
         }
-        if(isFull()) {
-           expandArray();
+        if (isFull()) {
+            expandArray();
         }
         //Bucle para mover los valores a la derecha.
-       moveToRight(index);
-       //Inserta el valor en el indice indicado.
-       data[index] = value;
-       return true;
+        moveToRight(index);
+        //Inserta el valor en el indice indicado.
+        data[index] = value;
+        return true;
     }
 
+    /**
+     * Método privado que mueve todos los elementos del array hacia la derecha a partir del índice especificado.
+     *
+     * @param index el índice a partir del cual se deben mover los elementos hacia la derecha.
+     */
     private void moveToRight(int index) {
-        for(int i = elementCount; i > index; i--) {
+        for (int i = elementCount; i > index; i--) {
             data[i] = data[i - 1];
         }
         elementCount++;
     }
 
+    /**
+     * Método privado que mueve todos los elementos del array hacia la izquierda a partir del índice especificado.
+     *
+     * @param index el índice a partir del cual se deben mover los elementos hacia la izquierda.
+     */
     private void moveToLeft(int index) {
-        for(int i = index; i < elementCount - 1; i++) {
+        for (int i = index; i < elementCount - 1; i++) {
             data[i] = data[i + 1];
         }
         elementCount--;
@@ -73,12 +114,13 @@ public class GenericDynamicArray<T> {
     /**
      * Método que recibe un índice a encontrar y mueve todos los elementos a la izquierda reemplazando
      * el valor buscado y decrementando el tamaño del array.
+     *
      * @param index índice int.
      * @return devuelve el valor del índice bbuscado en double.
      */
     @SuppressWarnings("unchecked")
     public T remove(int index) {
-        if(index >= elementCount || index < 0) {
+        if (index >= elementCount || index < 0) {
             return null;
         }
         T removedValueFromIndex = (T) data[index];
@@ -91,14 +133,15 @@ public class GenericDynamicArray<T> {
      * Primero itera y busca la primera ocurrencia del valor, luego mueve todos los valores
      * de la derecha a la izaquierda sobreescribiendo el valor buscado.
      * El valor que se busca se guarda en una variable.
+     *
      * @param value double, valor a eliminar.
      * @return el valor que se busca y se quita del array.
      */
     @SuppressWarnings("unchecked")
     public T remove(T value) {
         T removedValue = null;
-        for(int i = 0; i < elementCount; i++) {
-            if(data[i] == value) {
+        for (int i = 0; i < elementCount; i++) {
+            if (data[i] == value) {
                 removedValue = (T) data[i];
                 moveToLeft(i);
                 return removedValue;
@@ -109,12 +152,13 @@ public class GenericDynamicArray<T> {
 
     /**
      * Método que obtiene el valor del array en el índice que se le pasa
+     *
      * @param index índice buscado.
      * @return el valor del índice buscado.
      */
     @SuppressWarnings("unchecked")
     public T get(int index) {
-        if(index >= elementCount || index < 0) {
+        if (index >= elementCount || index < 0) {
             return null;
         }
         return (T) data[index];
@@ -122,12 +166,13 @@ public class GenericDynamicArray<T> {
 
     /**
      * Method que introduce el valor al índice indicado sobreescribiendo el valor existente.
+     *
      * @param index int
      * @param value valor
      * @return true si se ha sobreescrito correctamente.
      */
     public boolean set(int index, T value) {
-        if(index >= elementCount || index < 0) {
+        if (index >= elementCount || index < 0) {
             return false;
         }
         data[index] = value;
@@ -135,24 +180,28 @@ public class GenericDynamicArray<T> {
     }
 
     /**
-     * Método que comprueba si el array está lleno o no.
-     * @return true si está lleno y false si no está lleno.
+     * Método privado que verifica si el array está lleno.
+     *
+     * @return true si el array está lleno, false si no.
      */
     private boolean isFull() {
         return elementCount == data.length;
     }
 
+    /**
+     * Método que devuelve un entero con el tamaño de array.
+     * @return devuelve un entero con el numero de elementos del array.
+     */
     public int size() {
         return elementCount;
     }
 
     /**
-     * Método para expandir el array si está lleno, se amplia el doble de la cantidad inicial.
-     * Finalmente se apunta el array expandido al array original para realizar la copia.
+     * Método para expandir el array si está lleno, duplicando su capacidad.
      */
     private void expandArray() {
         Object[] arrayExpanded = new Object[Math.round(data.length * GROW_FACTOR)];
-        for(int i = 0; i < elementCount; i++) {
+        for (int i = 0; i < elementCount; i++) {
             arrayExpanded[i] = data[i];
         }
         data = arrayExpanded;
@@ -166,8 +215,8 @@ public class GenericDynamicArray<T> {
         GenericDynamicArray that = (GenericDynamicArray) o;
 
         if (elementCount != that.elementCount) return false;
-        for(int i = 0; i < elementCount; i++) {
-            if(data[i] != that.data[i]) {
+        for (int i = 0; i < elementCount; i++) {
+            if (data[i] != that.data[i]) {
                 return false;
             }
         }
@@ -180,7 +229,7 @@ public class GenericDynamicArray<T> {
         // Habría que crear una función hash que solo tuviese en cuenta
         // los primeros tope - 1 elementos.
         Object[] effectiveArray = new Object[elementCount];
-        for(int i = 0; i < effectiveArray.length; i++) {
+        for (int i = 0; i < effectiveArray.length; i++) {
             effectiveArray[i] = data[i];
         }
         int result = Arrays.hashCode(effectiveArray);
@@ -192,7 +241,7 @@ public class GenericDynamicArray<T> {
     public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append("[ ");
-        for(int i = 0; i < elementCount; i++) {
+        for (int i = 0; i < elementCount; i++) {
             sb.append(data[i]).append("\n");
         }
         sb.append(" ]");
--- src/main/java/com/denniseckerskorn/ejerciciosexcepciones/Ejer02.java
+++ src/main/java/com/denniseckerskorn/ejerciciosexcepciones/Ejer02.java
@@ -5,27 +5,48 @@ import com.denniseckerskorn.dynamicarray.GenericDynamicArray;
 import java.util.Scanner;
 
 public class Ejer02 {
-    private Scanner lector;
     private GenericDynamicArray<Double> decimalNumberArray;
+
     public Ejer02() {
-        requestDecimalNumbers("Introduce 10 decimal numbers", 10);
-        decimalNumberArray.toString();
+        int errorCounter = requestDecimalNumbers("Introduce 10 decimal numbers", 10);
+        System.out.println(decimalNumberArray.toString());
+        double maxDecimalValue = decimalNumberArray.getMaxValue();
+        System.out.println("Max Value is: " + maxDecimalValue);
+        System.out.println("Error counts: " + errorCounter);
 
     }
-    public void requestDecimalNumbers(String message, int quantity) {
-        lector = new Scanner(System.in);
+
+    /**
+     * Method to read decimal numbers, where the quantity needs to be indicated.
+     * Keeps track of the number of inputs made.
+     * Counts how many exceptions have occurred.
+     *
+     * @param message  the message to display when prompting for input
+     * @param quantity the number of decimal numbers to read
+     * @return the number of exceptions that occurred during input
+     */
+    public int requestDecimalNumbers(String message, int quantity) {
+        Scanner lector = new Scanner(System.in);
         decimalNumberArray = new GenericDynamicArray<>(quantity);
         double input = 0;
         int errorCount = 0;
-        //Falta sacar el maximo de los numeros. Metodo
-        for(int i = 0; i <= quantity; i++) {
-            try{
+        int numberCount = 0;
+        boolean exit = false;
+        do {
+            try {
+                System.out.println(message);
                 input = Double.parseDouble(lector.nextLine());
                 decimalNumberArray.add(input);
+                numberCount++;
+                if (numberCount == quantity) {
+                    exit = true;
+                }
             } catch (NumberFormatException nfe) {
-                System.out.println("You can only introduce numbers");
+                System.out.println("Only decimal numbers are allowed");
                 errorCount++;
             }
-        }
+        } while (!exit);
+        lector.close();
+        return errorCount;
     }
 }
