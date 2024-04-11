7dfb1d7cf9cb537ca852913720308fed5f768f14
DennisEckerskorn <45369024+DennisEckerskorn@users.noreply.github.com>
2024-04-12 00:16:26 +0200

    Update exception handling

--- src/main/java/com/denniseckerskorn/ejerciciosexcepciones/Ejer03.java
+++ src/main/java/com/denniseckerskorn/ejerciciosexcepciones/Ejer03.java
@@ -2,17 +2,29 @@ package com.denniseckerskorn.ejerciciosexcepciones;
 
 import com.denniseckerskorn.dynamicarray.GenericDynamicArray;
 
+import java.nio.channels.ScatteringByteChannel;
 import java.util.Scanner;
 
 public class Ejer03 {
     private GenericDynamicArray<Double> data;
 
     public Ejer03() {
-        requestNumbersIntoArray("Please introduce numbers");
-        double maxValue = data.getMaxValue();
-        System.out.println("Max Value: " + maxValue);
+        try {
+            requestNumbersIntoArray("Please introduce numbers");
+            if (data != null && data.size() != 0) {
+                double maxValue = 0;
+                maxValue = data.getMaxValue();
+                System.out.println("Max Value: " + maxValue);
 
+            } else {
+                System.out.println("No numbers were provided");
+            }
+        } catch (NullPointerException npe) {
+            System.out.println("Input can't be empty, please introduce a number.");
+
+        }
     }
+
 
     public void requestNumbersIntoArray(String message) {
         Scanner lector = new Scanner(System.in);
@@ -25,10 +37,8 @@ public class Ejer03 {
                 System.out.println(message);
                 input = Double.parseDouble(lector.nextLine());
                 data.add(input);
-            } catch (NullPointerException npe) {
-                System.out.println("Input can't be empty, please introduce a number.");
             } catch (NumberFormatException nfe) {
-                System.out.println("Sorry, only numbers... Exit");
+                System.out.println("Only numbers can be introduced...Exit");
                 exit = true;
             }
         } while (!exit);
