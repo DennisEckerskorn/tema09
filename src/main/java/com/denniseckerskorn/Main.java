38f5e2d09b25419008ba2129c926b50a1e1699cc
Dennis Eckerskorn <denniseckerskorn@gmail.com>
2024-04-14 13:01:06 +0200

    Created methods for ejer06

--- src/main/java/com/denniseckerskorn/Main.java
+++ src/main/java/com/denniseckerskorn/Main.java
@@ -1,9 +1,6 @@
 package com.denniseckerskorn;
 
-import com.denniseckerskorn.ejerciciosexcepciones.Ejer01;
-import com.denniseckerskorn.ejerciciosexcepciones.Ejer02;
-import com.denniseckerskorn.ejerciciosexcepciones.Ejer03;
-import com.denniseckerskorn.ejerciciosexcepciones.Ejer04;
+import com.denniseckerskorn.ejerciciosexcepciones.*;
 import com.denniseckerskorn.ejerciciosexcepciones.ejer05.Ejer05;
 
 public class Main {
@@ -12,7 +9,8 @@ public class Main {
         //Ejer02 ejer02 = new Ejer02();
         //Ejer03 ejer03 = new Ejer03();
         //Ejer04 ejer04 = new Ejer04();
-        Ejer05 ejer05 = new Ejer05();
+        //Ejer05 ejer05 = new Ejer05();
+        Ejer06 ejer06 = new Ejer06();
 
     }
 }
--- src/main/java/com/denniseckerskorn/ejerciciosexcepciones/Ejer06.java
+++ src/main/java/com/denniseckerskorn/ejerciciosexcepciones/Ejer06.java
@@ -1,7 +1,51 @@
 package com.denniseckerskorn.ejerciciosexcepciones;
 
+import java.util.Arrays;
+
 public class Ejer06 {
     public Ejer06() {
+        int[] array = {-2, -1, 0, 1, 2};
 
+        int[] arrayResult = dividirEntreArrayV3(2, array);
+
+        System.out.println(Arrays.toString(arrayResult));
+
+    }
+
+    public int[] dividirEntreArray(int num, int[] array) {
+        int[] resultadoDivision = new int[array.length];
+
+        for (int i = 0; i < array.length; i++) {
+            resultadoDivision[i] = num / array[i];
+        }
+
+        return resultadoDivision;
+    }
+
+    public int[] dividirEntreArrayV2(int num, int[] array) {
+        int[] resultadoDivision = new int[array.length];
+
+        for (int i = 0; i < array.length; i++) {
+            try {
+                resultadoDivision[i] = num / array[i];
+
+            } catch (ArithmeticException ae) {
+                System.out.println("0 no puede ser dividido por 0");
+            }
+        }
+        return resultadoDivision;
+    }
+
+    public int[] dividirEntreArrayV3(int num, int[] array) {
+        int[] resultadoDivision = new int[array.length];
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != 0) {
+                resultadoDivision[i] = num / array[i];
+            } else {
+                resultadoDivision[i] = 0;
+            }
+        }
+        return resultadoDivision;
     }
 }
