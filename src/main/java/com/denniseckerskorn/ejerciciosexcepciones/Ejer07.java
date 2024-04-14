a849af3d52d4fcd9d9a7e46743d747436f456546
Dennis Eckerskorn <denniseckerskorn@gmail.com>
2024-04-14 13:16:14 +0200

    Reformated code

--- src/main/java/com/denniseckerskorn/ejerciciosexcepciones/Ejer07.java
+++ src/main/java/com/denniseckerskorn/ejerciciosexcepciones/Ejer07.java
@@ -10,13 +10,14 @@ public class Ejer07 {
 
     /**
      * Método que devuelve un String con los primeros carácteres de la cadena de cada posicion del Array de Strings
+     *
      * @param array de String[]
      * @return devuelve un StringBuilder con los carácteres de la primera posición de la cadena.
      */
     public String mostrarCadenasArray(String[] array) {
         StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < array.length; i++) {
-            if(array[i] != null) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != null) {
                 sb.append(array[i].charAt(0));
             }
         }
